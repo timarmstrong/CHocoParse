436d52e9dbed4934ab18b83aa97a48ab0712cce4
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-07-11 12:57:31 -0500

    Split out tokens from lexer interface.  Work on sketching event-driven reader interface

--- src/tsconfig_lex.h
+++ src/tsconfig_lex.h
@@ -13,58 +13,12 @@
 #include <stdbool.h>
 
 #include "tsconfig.h"
+#include "tsconfig_tok.h"
 
 typedef struct {
   bool include_ws_str : 1; // Include whitespace string
   bool include_comm_str : 1; // Include comment strings
 } tscfg_lex_opts;
-
-typedef enum {
-  /* Special tokens: don't include string */
-  TSCFG_TOK_INVALID = 0, // Special tag for invalid token
-  TSCFG_TOK_EOF, // Special token for end of file
-
-  /* Special whitespace tokens: include string */
-  TSCFG_TOK_WS, // Whitespace without newline
-  TSCFG_TOK_WS_NEWLINE, // Whitespace with newline
-
-  /* Comment: don't include string */
-  TSCFG_TOK_COMMENT,
-
-  /* Paired punctuation tokens: don't include string */
-  TSCFG_TOK_OPEN_BRACE,
-  TSCFG_TOK_CLOSE_BRACE,
-  TSCFG_TOK_OPEN_PAREN,
-  TSCFG_TOK_CLOSE_PAREN,
-  TSCFG_TOK_OPEN_SQUARE,
-  TSCFG_TOK_CLOSE_SQUARE,
-
-  /* Punctuation: don't include string */
-  TSCFG_TOK_COMMA,
-  TSCFG_TOK_EQUAL,
-  TSCFG_TOK_PLUSEQUAL,
-  TSCFG_TOK_COLON,
-
-  /* Keywords: don't include string */
-  TSCFG_TOK_TRUE,
-  TSCFG_TOK_FALSE,
-  TSCFG_TOK_NULL,
-
-  /* Literals and variables: include string */
-  TSCFG_TOK_NUMBER, // Numeric token, text is stored in str
-  TSCFG_TOK_UNQUOTED, // Unquoted text, text is stored in str
-  TSCFG_TOK_STRING, // Quoted text, string contents after escaping, etc
-  TSCFG_TOK_VAR, // Variable name, name store in string
-} tscfg_tok_tag;
-
-/*
- * Lexer token
- */
-typedef struct {
-  tscfg_tok_tag tag;
-  char *str;
-  size_t length;
-} tscfg_tok;
 
 /*
  * Lexing state.
---
+++ src/tsconfig_tok.h
@@ -0,0 +1,60 @@
+/*
+ * Lexer tokens for HOCON properties file.
+ *
+ * Author: Tim Armstrong
+ * tim.g.armstrong@gmail.com
+ * Created: 11 July 2014
+ * All rights reserved.
+ */
+
+#ifndef __TSCONFIG_TOK_H
+#define __TSCONFIG_TOK_H
+
+typedef enum {
+  /* Special tokens: don't include string */
+  TSCFG_TOK_INVALID = 0, // Special tag for invalid token
+  TSCFG_TOK_EOF, // Special token for end of file
+
+  /* Special whitespace tokens: include string */
+  TSCFG_TOK_WS, // Whitespace without newline
+  TSCFG_TOK_WS_NEWLINE, // Whitespace with newline
+
+  /* Comment: don't include string */
+  TSCFG_TOK_COMMENT,
+
+  /* Paired punctuation tokens: don't include string */
+  TSCFG_TOK_OPEN_BRACE,
+  TSCFG_TOK_CLOSE_BRACE,
+  TSCFG_TOK_OPEN_PAREN,
+  TSCFG_TOK_CLOSE_PAREN,
+  TSCFG_TOK_OPEN_SQUARE,
+  TSCFG_TOK_CLOSE_SQUARE,
+
+  /* Punctuation: don't include string */
+  TSCFG_TOK_COMMA,
+  TSCFG_TOK_EQUAL,
+  TSCFG_TOK_PLUSEQUAL,
+  TSCFG_TOK_COLON,
+
+  /* Keywords: don't include string */
+  TSCFG_TOK_TRUE,
+  TSCFG_TOK_FALSE,
+  TSCFG_TOK_NULL,
+
+  /* Literals and variables: include string */
+  TSCFG_TOK_NUMBER, // Numeric token, text is stored in str
+  TSCFG_TOK_UNQUOTED, // Unquoted text, text is stored in str
+  TSCFG_TOK_STRING, // Quoted text, string contents after escaping, etc
+  TSCFG_TOK_VAR, // Variable name, name stored in string
+} tscfg_tok_tag;
+
+/*
+ * Lexer token
+ */
+typedef struct {
+  tscfg_tok_tag tag;
+  char *str;
+  size_t length;
+} tscfg_tok;
+
+#endif // __TSCONFIG_TOK_H
